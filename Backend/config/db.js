d13b781f0de4ba4af3643826400d8e7c19ea5a0a
venom827 <gvenom680@gmail.com>
2025-11-05 10:16:16 +0000

    domne

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -2,11 +2,11 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log("✅ MongoDB Connected");
+    console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
     console.error("❌ MongoDB connection failed:", error.message);
     process.exit(1);
